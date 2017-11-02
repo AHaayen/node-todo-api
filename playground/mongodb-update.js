48b20387dc1bd983db48fee95e39ded793eabc98
AndresHaayen <andreshaayen@gmail.com>
2017-11-02 12:40:07 +0100

    Add update script. Learned how to update a database document with nodejs

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,7 +8,35 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongDB server');
 
-    
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('59faf4f274190500c7bbf7e7')
+    // }, {
+    //     $set: {
+    //         completed: true
+
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('59f85836e9add54346b21579')
+    }, {
+        $set: {
+            name: 'Luna Sophia Haayen'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+
 
 //db.close();
 
