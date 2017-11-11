88cec907368bc7232d6f3faaf573441e3568a9aa
AndresHaayen <andreshaayen@gmail.com>
2017-11-11 23:00:40 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
-var { mongoose } = require('./db/mongoose.js');
-var { Todo } = require('./models/todo');
-var { user1 } = require('./models/user');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose.js');
+const { Todo } = require('./models/todo');
+const { user1 } = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -63,6 +65,35 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo){
             return res.status(404).send();
         }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+
+// Pacth is what u use when u wanna update resource
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']); 
+
+    if (!ObjectID.isValid(id)) { // validate the id -> not valid? return 404
+        return res.status(404).send();
+    };
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
